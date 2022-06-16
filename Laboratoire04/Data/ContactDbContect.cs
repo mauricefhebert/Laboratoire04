c995ee5facd9783053ff589c71e4e58eec041e4b
mauricefh <filiatreaultmaurice@gmail.com>
2022-06-16 16:40:55 -0400

    Shell Layout Implimentation

---
+++ Laboratoire04/Data/ContactDbContect.cs
@@ -0,0 +1,13 @@
+﻿using Laboratoire04.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Laboratoire04.Data
+{
+    internal class ContactDbContect
+    {
+        public ObservableCollection<Contact> contacts = new ObservableCollection<Contact>();
+    }
+}
---
+++ Laboratoire04/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratoire04.Models
+{
+    internal class Contact
+    {
+        public int Id { get; set; }
+        public string Prenom { get; set; }
+        public string Nom { get; set; }
+        public string Initital { get; set; }
+        public string Photo { get; set; }
+        public string CourrielPersonnel { get; set; }
+        public string CourrielTravail { get; set; }
+        public string TelephonePersonnel { get; set; }
+        public string TelephoneTravail { get; set; }
+    }
+}
