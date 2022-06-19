0ebf2d1516828a9addd0bebe448c77c7d5068108
mauricefhebert <filiatreaultmaurice@gmail.com>
2022-06-19 17:08:54 -0400

    Added the viewmodel folder and done the binding context

--- Laboratoire04/Views/ListContactView.xaml.cs
+++ Laboratoire04/Views/ListContactView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Laboratoire04.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace Laboratoire04.Views
         public ListContactView()
         {
             InitializeComponent();
+            this.BindingContext = new ListContactViewModel();
         }
     }
 }
