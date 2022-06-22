c8c544d21e1f5a4713d540c04e67f188808b9d07
mauricefhebert <filiatreaultmaurice@gmail.com>
2022-06-22 11:40:12 -0400

    update

--- Laboratoire04/ViewModels/DetailsContactViewModel.cs
+++ Laboratoire04/ViewModels/DetailsContactViewModel.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Web;
 using System.Windows.Input;
@@ -11,20 +12,101 @@ using Xamarin.Forms;
 
 namespace Laboratoire04.ViewModels
 {
-    [QueryProperty(nameof(Id), "Id")]
     internal class DetailsContactViewModel : INotifyPropertyChanged, IQueryAttributable
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public int Id { get; set; }
-        public string Prenom { get; set; }
-        public string Nom { get; set; }
-        public string Initial { get; set; }
-        public string Photo { get; set; }
-        public string CourrielPersonnel { get; set; }
-        public string CourrielTravail { get; set; }
-        public string TelephonePersonnel { get; set; }
-        public string TelephoneTravail { get; set; }
+        private int id;
+        public int Id 
+        { 
+            get { return id; } 
+            set
+            {
+                id = value;
+                OnProperyChanged();
+            }
+        }
+
+        private string prenom;
+        public string Prenom 
+        { 
+            get { return prenom; }
+            set
+            {
+                prenom = value;
+                OnProperyChanged();
+            }
+        }
+        private string nom;
+        public string Nom 
+        { 
+            get { return nom; }
+            set
+            {
+                nom = value;
+                OnProperyChanged();
+            }
+        }
+        private string initial;
+        public string Initial 
+        {   get { return initial; }
+            set
+            {
+                initial = value;
+                OnProperyChanged();
+            }
+        }
+
+        private string photo;
+        public string Photo
+        {
+            get { return photo; }
+            set
+            {
+                photo = value;
+                OnProperyChanged();
+            }
+        }
+        private string telephonePersonnel;
+        public string TelephonePersonnel
+        {
+            get { return telephonePersonnel; }
+            set
+            {
+                telephonePersonnel = value;
+                OnProperyChanged();
+            }
+        }
+        private string telephoneTravail;
+        public string TelephoneTravail
+        {
+            get { return telephoneTravail; }
+            set
+            {
+                telephoneTravail = value;
+                OnProperyChanged();
+            }
+        }
+        private string courrielPersonnel;
+        public string CourrielPersonnel
+        {
+            get { return courrielPersonnel; }
+            set
+            {
+                courrielPersonnel = value;
+                OnProperyChanged();
+            }
+        }
+        private string courrielTravail;
+        public string CourrielTravail
+        {
+            get { return courrielTravail; }
+            set
+            {
+                courrielTravail = value;
+                OnProperyChanged();
+            }
+        }
         public ICommand EditCmd { get; set; }
 
         public void ApplyQueryAttributes(IDictionary<string, string> query)
@@ -36,32 +118,31 @@ namespace Laboratoire04.ViewModels
         void LoadContact(string id)
         {
             Contact contact = ContactDbContext.GetContacts().Find(c => c.Id == int.Parse(id));
+            this.Id = contact.Id;
             this.Prenom = contact.Prenom;
             this.Nom = contact.Nom;
             this.Initial = contact.Initial;
             this.Photo = contact.Photo;
-            this.CourrielPersonnel = contact.CourrielPersonnel;
-            this.CourrielTravail = contact.CourrielTravail;
             this.TelephonePersonnel = contact.TelephonePersonnel;
             this.TelephoneTravail = contact.TelephoneTravail;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Prenom)));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Nom)));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Initial)));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Photo)));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CourrielPersonnel)));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CourrielTravail)));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TelephonePersonnel)));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TelephoneTravail)));
+            this.CourrielPersonnel = contact.CourrielPersonnel;
+            this.CourrielTravail = contact.CourrielTravail;
         }
 
         public DetailsContactViewModel()
         {
             EditCmd = new Command(EditContact);
+            
         }
 
         public void EditContact()
         {
 
+        }
+
+        public void OnProperyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
