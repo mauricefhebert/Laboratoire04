6ce272bbc0fce149f53b701e070b7bcbc6ceac2a
mauricefh <filiatreaultmaurice@gmail.com>
2022-06-23 02:52:14 -0400

    update

--- Laboratoire04/Views/DetailsContactView.xaml.cs
+++ Laboratoire04/Views/DetailsContactView.xaml.cs
@@ -18,26 +18,5 @@ namespace Laboratoire04.Views
             InitializeComponent();
             this.BindingContext = new DetailsContactViewModel();
         }
-
-        private void ToolBarEditMenu_Clicked(object sender, EventArgs e)
-        {
-            var GridContainer = DetailsContactContainerGrid.Children;
-            foreach (var frame in GridContainer)
-            {
-                if(frame is Frame)
-                {
-                    foreach(var entry in ((Frame)frame)) // Get children of frame here
-                    {
-                        if (entry is Entry)
-                        {
-                            if (!(entry as Entry).IsEnabled)
-                            {
-                                (entry as Entry).IsEnabled = true;
-                            }
-                        }
-                    }
-                }
-            }
-        }
     }
 }
