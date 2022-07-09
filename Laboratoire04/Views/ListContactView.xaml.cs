4527f24f04f481e7b3a742d716b15465456becfb
mauricefhebert <filiatreaultmaurice@gmail.com>
2022-07-09 16:10:36 -0400

    update

--- Laboratoire04/Views/ListContactView.xaml.cs
+++ Laboratoire04/Views/ListContactView.xaml.cs
@@ -36,68 +36,57 @@ namespace Laboratoire04.Views
             {
                 ContactDbContext.SeedDatabase(new Contact()
                 {
-                    Prenom = "Prenom 01",
-                    Nom = "Nom 01",
-                    Initial = "PN 01",
-                    Photo = @"https://placeimg.com/150/150/people",
-                    TelephonePersonnel = "(555) 555-5555",
+                    Prenom = "Noreen",
+                    Nom = "Cremer",
+                    Initial = "NC",
+                    Photo = @"https://this-person-does-not-exist.com/img/avatar-97719ae2aac2eff173d8f210a7211c81.jpg",
+                    TelephonePersonnel = "(924) 897-5338",
+                    TelephoneTravail = "(333) 333-3333",
+                    CourrielPersonnel = "ncremer1@mozilla.org",
+                    CourrielTravail = "CourrielTravail01@hotmail.com"
+                });
+                ContactDbContext.SeedDatabase(new Contact()
+                {
+                    Prenom = "Frazer",
+                    Nom = "Garretts",
+                    Initial = "FG",
+                    Photo = @"https://this-person-does-not-exist.com/img/avatar-3d29ae993ae8332a7585a06ba25cbdd2.jpg",
+                    TelephonePersonnel = "(282) 860-6300",
                     TelephoneTravail = "(333) 333-3333",
                     CourrielPersonnel = "CourrielPersonnel01@hotmail.com",
                     CourrielTravail = "CourrielTravail01@hotmail.com"
                 });
                 ContactDbContext.SeedDatabase(new Contact()
                 {
-                    Prenom = "Prenom 01",
-                    Nom = "Nom 01",
-                    Initial = "PN 01",
-                    Photo = @"https://placeimg.com/150/150/people",
-                    TelephonePersonnel = "(555) 555-5555",
+                    Prenom = "Nikolaus",
+                    Nom = "Dashkovich",
+                    Initial = "ND",
+                    Photo = @"https://this-person-does-not-exist.com/img/avatar-128240b8ab415dbc1f66845c372c12f1.jpg",
+                    TelephonePersonnel = "(733) 723-3469",
                     TelephoneTravail = "(333) 333-3333",
-                    CourrielPersonnel = "CourrielPersonnel01@hotmail.com",
+                    CourrielPersonnel = "ndashkovich3@mtv.com",
                     CourrielTravail = "CourrielTravail01@hotmail.com"
                 });
                 ContactDbContext.SeedDatabase(new Contact()
                 {
-                    Prenom = "Prenom 01",
-                    Nom = "Nom 01",
-                    Initial = "PN 01",
-                    Photo = @"https://placeimg.com/150/150/people",
-                    TelephonePersonnel = "(555) 555-5555",
+                    Prenom = "Dorolice",
+                    Nom = "Alf",
+                    Initial = "DA",
+                    Photo = @"https://this-person-does-not-exist.com/img/avatar-52bd5b0d48d84c43e8498c8d2e60484f.jpg",
+                    TelephonePersonnel = "(309) 351-8274",
                     TelephoneTravail = "(333) 333-3333",
-                    CourrielPersonnel = "CourrielPersonnel01@hotmail.com",
+                    CourrielPersonnel = "dalf4@creativecommons.org",
                     CourrielTravail = "CourrielTravail01@hotmail.com"
                 });
                 ContactDbContext.SeedDatabase(new Contact()
                 {
-                    Prenom = "Prenom 01",
-                    Nom = "Nom 01",
-                    Initial = "PN 01",
-                    Photo = @"https://placeimg.com/150/150/people",
-                    TelephonePersonnel = "(555) 555-5555",
+                    Prenom = "Estevan",
+                    Nom = "Gatch",
+                    Initial = "EG",
+                    Photo = @"https://this-person-does-not-exist.com/img/avatar-a0e73a11ab38784298b031db724a69b5.jpg",
+                    TelephonePersonnel = "(295) 268-1091",
                     TelephoneTravail = "(333) 333-3333",
-                    CourrielPersonnel = "CourrielPersonnel01@hotmail.com",
-                    CourrielTravail = "CourrielTravail01@hotmail.com"
-                });
-                ContactDbContext.SeedDatabase(new Contact()
-                {
-                    Prenom = "Prenom 01",
-                    Nom = "Nom 01",
-                    Initial = "PN 01",
-                    Photo = @"https://placeimg.com/150/150/people",
-                    TelephonePersonnel = "(555) 555-5555",
-                    TelephoneTravail = "(333) 333-3333",
-                    CourrielPersonnel = "CourrielPersonnel01@hotmail.com",
-                    CourrielTravail = "CourrielTravail01@hotmail.com"
-                });
-                ContactDbContext.SeedDatabase(new Contact()
-                {
-                    Prenom = "Prenom 01",
-                    Nom = "Nom 01",
-                    Initial = "PN 01",
-                    Photo = @"https://placeimg.com/150/150/people",
-                    TelephonePersonnel = "(555) 555-5555",
-                    TelephoneTravail = "(333) 333-3333",
-                    CourrielPersonnel = "CourrielPersonnel01@hotmail.com",
+                    CourrielPersonnel = "egatch2@istockphoto.com",
                     CourrielTravail = "CourrielTravail01@hotmail.com"
                 });
             }
